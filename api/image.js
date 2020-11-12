2fd5296b3e01dd7198b65f06a9200982b0ae5fc9
skanaar <daniel.kallin@gmail.com>
2020-11-12 09:11:08 +0100

    trying vercel

---
+++ api/image.js
@@ -0,0 +1,16 @@
+const nomnoml = require('../dist/nomnoml');
+
+const badInput = nomnoml.renderSvg('bad input')
+
+module.exports = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'image/svg+xml'})
+  try {
+    var input = req.url.split('source=')[1]
+    if (!input) throw new Error()
+    var source = decodeURIComponent(input)
+    res.end(nomnoml.renderSvg(source))
+  }
+  catch (e) {
+    res.end(badInput)
+  }
+}
